0460366a96b0c0cdfe410c0817dad83fb9a97dfc
EtherD3v <118121669+EtherD3v@users.noreply.github.com>
2024-06-25 18:00:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-
+function talk(){
+  var know = {
+    "Quoi ?" : "Ordinateurs, tablettes, téléphones portables etc.",
+    "Pourquoi ?" : "Notre mission : vous aider à adopter le bon équipement informatique au quotidien.",
+    "Comment ?" : "Grâce à notre équipe de vendeurs et techniciens.",
+    "Quand ?" : "Les horaires sont inscris sur votre gauche.",
+    "Où ?" : "H&D 7, boutique d'informatique et de téléphonie. pour + d'infos, rendez vous sur notre FaceBook."
+  };
+  var user = document.getElementById('userBox').value;
+    document.getElementById('chatLog').innerHTML = user + "<br>";
+  if (user in know) {
+    document.getElementById('chatLog').innerHTML = know[user] + "<br>";
+  }else{
+    document.getElementById('chatLog').innerHTML = "Désolé, je ne comprends pas votre demande :( <br>";
+  }
+}
